32932964887d574153d9298d21f2abf0ebf24120
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-22 12:22:24 -0600

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+document.addEventListener("keydown" , e => {if(e.key === "ArrowLeft")moveDodgerLeft()})
+document.addEventListener("keydown" , e => {if(e.key === "ArrowRight")moveDodgerRight()})
+
+const dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#FF69B4"
+
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px" , "")
+    const left = parseInt(leftNumbers , 10)
+
+    if (left > 0){
+        dodger.style.left = `${left - 10}px`
+    }
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px" , "")
+    const left = parseInt(leftNumbers , 10)
+
+    if (left < 360){
+        dodger.style.left = `${left+10}px`
+    }
+}
